e52edd1043b8fd2aa84a34db165bb915e0549481
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-04-14 11:09:35 -0400

    using search form to test ng repeat on results

--- app/controllers/addysearch.controller.js
+++ app/controllers/addysearch.controller.js
@@ -6,4 +6,15 @@ function addySearchController(){
 	vm.street = "";
 	vm.city = "";
 	vm.zip = "";
+	vm.addresses = [];
+	vm.addAddress = addAddress;
+	function addAddress(){
+		var address = {
+			"street":vm.street,
+			"city":vm.city,
+			"zip":vm.zip
+		};
+		vm.addresses.push(address);
+	}
 }
+
