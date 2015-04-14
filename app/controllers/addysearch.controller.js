d6d51a13ce11dba993dcc1153f21173ff147c6ff
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-04-14 11:21:57 -0400

    using http service to retrieve json data

--- app/controllers/addysearch.controller.js
+++ app/controllers/addysearch.controller.js
@@ -1,6 +1,5 @@
 angular.module("pizzaland").controller("addySearchController", addySearchController); 
-function addySearchController(){
-
+function addySearchController($http){
 	var vm = this;
 	vm.message = "HOLA!";
 	vm.street = "";
@@ -8,6 +7,16 @@ function addySearchController(){
 	vm.zip = "";
 	vm.addresses = [];
 	vm.addAddress = addAddress;
+	vm.demoData = {};
+	$http.get('./data/demo-data.json').
+    success(function(data, status, headers, config) {
+      vm.demoData = data;
+      console.log(vm.demoData);
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
+
 	function addAddress(){
 		var address = {
 			"street":vm.street,
