863fd24691a8c014673f36bf54610370e4b34b15
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-04-15 12:02:29 -0400

    removed controller from app.js file

--- app/app.js
+++ app/app.js
@@ -2,11 +2,9 @@ var pizzaland = angular.module("pizzaland", ["ngRoute"]);
 
 pizzaland.config(['$routeProvider', function($routeProvider) {
  $routeProvider.when("/", {
-     controller: "addySearchController",
      templateUrl: '../../partials/addySearch.html'
  });
  $routeProvider.when("/order", {
-     controller: "orderFormController",
      templateUrl: '../../partials/orderForm.html'
   });
   $routeProvider.otherwise({redirectTo: '/'});
