627f91a8b23590f8f5967c27f459ffd8a19c2c9c
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-15 11:05:25 -0400

    trying to use $watchCollection to solve Async problem (not working)

--- app/controllers/addysearch.controller.js
+++ app/controllers/addysearch.controller.js
@@ -1,14 +1,18 @@
 angular.module("pizzaland").controller("addySearchController", addySearchController); 
-function addySearchController($http, householdFactory){
+function addySearchController($http, $scope, householdFactory){
 	var vm = this;
 	vm.street = "";
 	vm.city = "";
 	vm.zip = "";
 	vm.state = "";
-	
+	vm.addresses = householdFactory.addresses;
+
 	householdFactory.getDataFromJson();
 
-	vm.addresses = householdFactory.addresses;
+	$scope.$watchCollection(householdFactory.addresses, function(newVal, oldVal){
+		console.log(newVal, oldVal);
+	})
+
 
 	console.log(vm.addresses);
 }
