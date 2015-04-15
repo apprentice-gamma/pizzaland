43dddee42d53c8536c95ede6967139e7007306bc
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-15 14:36:39 -0400

    fixed jumbotron by referencing  in HTML

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 var pizzaland = angular.module("pizzaland", ["ngRoute"]);
 
 pizzaland.config(['$routeProvider', function($routeProvider) {
- $routeProvider.when("/", {
-     templateUrl: '../../partials/addySearch.html'
- });
- $routeProvider.when("/order", {
-     templateUrl: '../../partials/orderForm.html'
-  });
-  $routeProvider.otherwise({redirectTo: '/'});
-}])
+		$routeProvider.when("/", {
+ 		templateUrl: '../../partials/addySearch.html',
+		});
+
+	$routeProvider.when("/order", {
+	     templateUrl: '../../partials/orderForm.html',
+	});
+
+	$routeProvider.otherwise({redirectTo: '/'});
+}]);
--- app/controllers/display.controller.js
+++ app/controllers/display.controller.js
@@ -1,6 +1,5 @@
 angular.module("pizzaland").controller("displayController", displayController); 
 function displayController($http, $location, householdFactory){
 	var vm = this;
-	vm.showJumbotron = ($location.path() === '/');
-	console.log($location.path());
+	vm.location = $location;
 }
