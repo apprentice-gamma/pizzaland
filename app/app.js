5be6e47e0151955c8ceedfddcb4d7c793999f4fd
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-15 14:37:56 -0400

    fixing whitespace

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 var pizzaland = angular.module("pizzaland", ["ngRoute"]);
 
 pizzaland.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when("/", {
- 		templateUrl: '../../partials/addySearch.html',
-		});
+	$routeProvider.when("/", {
+		templateUrl: '../../partials/addySearch.html',
+	});
 
 	$routeProvider.when("/order", {
 	     templateUrl: '../../partials/orderForm.html',
