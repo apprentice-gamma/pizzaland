4581653e91b510ff7cc5d13a8344924701d18fa5
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-04-16 11:36:30 -0400

    damn you camelCase (fixing address population problem in orderForm)

--- app/controllers/orderForm.controller.js
+++ app/controllers/orderForm.controller.js
@@ -3,14 +3,13 @@ function orderFormController($http, householdFactory, $routeParams){
 	var vm = this;
 	vm.message = "This is the order form";
 	vm.houseID = $routeParams.id;
-	vm.householdFactory = householdFactory;
+	vm.households = householdFactory;
 	vm.houseObject = houseFinder();
+	
 	console.log(vm.houseObject);
-
+	
 	function houseFinder(){
-		console.log(householdFactory.addresses.length);
 		for (var i = 0; i < householdFactory.addresses.length; i++) {
-			console.log(householdFactory.addresses[i]._id);
 			if (householdFactory.addresses[i]._id === vm.houseID) {
 				return householdFactory.addresses[i];
 			}
